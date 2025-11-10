5fa98723ccda46aef7b59b0848512fefc901569a
annanazarova <a.nazarova@just-ai.com>
2025-11-10 19:18:23 +0500

    deleting unnecessary lines

--- src/test/java/tests/FaqTests.java
+++ src/test/java/tests/FaqTests.java
@@ -5,17 +5,12 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import pageObject.MainPage;
 
 import java.time.Duration;
-import java.util.List;
 import java.util.stream.Stream;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 
 public class FaqTests {
 
@@ -31,7 +26,7 @@ public class FaqTests {
     void faqListTest(int id, String answer) {
         driver.get("https://qa-scooter.praktikum-services.ru/");
         MainPage mainPage = new MainPage(driver);
-        // возможно ожидание не нужно, если страница быстро грузится
+        // добавлено ожидание, чтобы страница успевала прогрузиться
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(1));
         mainPage.acceptCookie();
         mainPage.clickFaqQuestionByNumber(id);
